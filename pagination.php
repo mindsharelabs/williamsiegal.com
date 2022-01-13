66eadedc226351e1185c0a0e9cd16a09006bc23b
James W. Johnson <me@thejameswilliam.com>
2022-01-13 12:49:42 -0700

    updated colors.

--- pagination.php
+++ pagination.php
@@ -3,7 +3,13 @@
 <div class="row">
   <div class="col-8 offset-2">
     <div class="pagination">
-      <?php mindwp_pagination(); ?>
+      <?php
+      if(function_exists('facetwp_display')) :
+        echo facetwp_display( 'facet', 'pagination' );
+      else :
+        mindwp_pagination();
+      endif;
+      ?>
     </div>
   </div>
 </div>
