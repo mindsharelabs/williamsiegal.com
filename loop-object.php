5304cd7eb240f12bb21c176e3055e4c9ce3c5104
James W. Johnson <me@thejameswilliam.com>
2022-06-21 12:36:50 -0600

    fiest image of image slides will be used if featured image does not exist.

--- loop-object.php
+++ loop-object.php
@@ -7,13 +7,16 @@ echo '<article id="post-' . get_the_id() . '" data-postID="' . get_the_id() . '"
     echo '<div class="image-container">';
       if(has_post_thumbnail( )) :
         the_post_thumbnail( 'thumbnal-square', array('class' => 'object-thumbnail'));
+      else :
+        $images = get_field('images');
+        echo wp_get_attachment_image( $images[0]['image']['ID'], 'thumbnal-square');
       endif;
     echo '</div>';
 
     echo '<div class="card-information">';
       echo '<h3 class="object-title text-white">' . get_the_title() . '</h3>';
       echo siegal_object_info(get_the_id());
-      
+
     echo '</div>';
   echo '</div>';
 echo '</article>';
